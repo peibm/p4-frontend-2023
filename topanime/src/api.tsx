4e2db2616605ced594037a3f20e44c374344413b
Pablo Mena <pablo.mena.rodriguez@gmail.com>
2023-07-03 22:02:01 +0200

    leverage topAnime code but using axios

--- topanime/src/api.tsx
+++ topanime/src/api.tsx
@@ -1,15 +1,91 @@
 import axios from "axios";
+export class Anime {
+  public rank: number;
+  public titles: Title[];
+  public images: Images;
+  public score: number;
+  public synopsis: string;
+  public year: number;
+  public episodes: number;
+  public airing: boolean;
+  public status: Status;
+  public broadcast: Broadcast;
+  public trailer: YoutubeVideo;
 
-const api = axios.create({
-  baseURL: "http://discord.jikan.moe/", 
-});
+  constructor(obj: any) {
+    this.rank = obj.rank;
+    this.titles = obj.titles;
+    this.images = obj.images;
+    this.score = obj.score;
+    this.synopsis = obj.synopsis;
+    this.year = obj.year;
+    this.episodes = obj.episodes;
+    this.airing = obj.airing;
+    this.status = obj.status;
+    this.broadcast = obj.broadcast;
+    this.trailer = obj.trailer;
+  }
+}
 
-export const fetchAnimeList = async () => {
-  const response = await api.get("/top/anime"); 
-  return response.data;
-};
+export enum Status {
+  finished = "Finished Airing",
+  airing = "Currently Airing",
+  complete = "Complete",
+  upcoming = "Not yet aired",
+}
 
-export const fetchAnimeDetails = async (id: string) => {
-  const response = await api.get(`/anime/${id}`); 
-  return response.data;
+export interface Broadcast {
+  day: string;
+  time: string;
+  timezone: string;
+  string: string;
+}
+
+export interface YoutubeVideo {
+  youtube_id: string;
+  url: string;
+  embed_url: string;
+}
+
+export interface Title {
+  type: string;
+  title: string;
+}
+
+export interface Images {
+  jpg: ImagesCollection;
+  webp?: ImagesCollection;
+}
+
+export interface ImagesCollection {
+  image_url: string;
+  small_image_url?: string;
+  medium_image_url?: string;
+  large_image_url?: string;
+  maximum_image_url?: string;
+}
+
+export interface AnimeQuery {
+  type: "tv" | "movie" | "ova" | "special" | "ona" | "music";
+  filter: "airing" | "upcoming" | "bypopularity" | "favorite";
+  page: number;
+  limit: number;
+}
+
+export const getTopAnimes = async (q: AnimeQuery) => {
+  const url = `https://api.jikan.moe/v4/top/anime?q=${q.type}/${q.filter}/${q.page}/${q.limit}`;
+
+  try {
+    const response = await axios.get(url);
+    const { data } = response.data as { data: any[] };
+    let animes: Array<Anime> = [];
+
+    for (const anime_all of data) {
+      animes.push(new Anime(anime_all));
+    }
+    return animes;
+  } catch (error) {
+    console.error(`Error fetching top animes: ${error}`);
+    return [];
+  }
 };
--- topanime/src/pages/HomePage.tsx
+++ topanime/src/pages/HomePage.tsx
@@ -1,13 +1,30 @@
 import React, { useEffect, useState } from "react";
-import { fetchAnimeList } from "../api";
+import { Anime, AnimeQuery, getTopAnimes } from "../api";
 import AnimeCard from "../components/AnimeCard";
 
+
+
 const HomePage = () => {
+  const [data, setData] = useState<Anime[] | string>("loading...");
+  const query: AnimeQuery = {
+    type: "tv",
+    filter: "bypopularity",
+    page: 1,
+    limit: 1,
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getTopAnimes(query);;
+      setData(data);
+    };
 
+    fetchData();
+  },[]);
 
-  return (
+  return <>
     <h1>HomePage</h1>
-  );
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+  </>;
 };
 
 export default HomePage;
